1a9e1848744682af246b46c2b14922dec0d8f844
matheusburey <matheushburei.99@gmail.com>
2025-03-09 10:54:21 -0300

    cofig base

---
+++ app/src/main/java/com/matheus/nearby/ui/components/NearByButton.kt
@@ -0,0 +1,9 @@
+package com.matheus.nearby.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NearByButton(modifier: Modifier = Modifier) {
+
+}
