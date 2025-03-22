6bde901419025c5b14cc43a9b4fd3aa9464f701b
matheusburey <matheushburei.99@gmail.com>
2025-03-22 13:24:41 -0300

    feat: create welcome screen

--- app/src/main/java/com/matheus/nearby/ui/components/welcome/WelcomeContent.kt
+++ app/src/main/java/com/matheus/nearby/ui/components/welcome/WelcomeContent.kt
@@ -1,40 +1,46 @@
 package com.matheus.nearby.ui.components.welcome
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.matheus.nearby.R
 import com.matheus.nearby.ui.theme.Typography
 
 @Composable
-fun WelcomeHeader(modifier: Modifier = Modifier) {
+fun WelcomeContent(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.spacedBy(24.dp)
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.img_logo_logo_icon),
-            contentDescription = "icon"
+        Text(text = "see how it works:", style = Typography.bodyLarge)
+        WelcomeHowItWorksTip(
+            modifier = Modifier.fillMaxWidth(),
+            title = "Find establishments",
+            description = "See places near you that are Nearby partners",
+            iconRes = R.drawable.ic_map_pin
         )
-        Spacer(modifier = Modifier.height(24.dp))
-        Text(text = "Welcome Nearby", style = Typography.headlineLarge)
-        Text(
-            text = "Get coupons to use at your favorite establishments",
-            style = Typography.bodyLarge
+        WelcomeHowItWorksTip(
+            modifier = Modifier.fillMaxWidth(),
+            title = "Activate the coupon with QR Code",
+            description = "Scan the code at the establishment to use the benefit",
+            iconRes = R.drawable.ic_qrcode
+        )
+        WelcomeHowItWorksTip(
+            modifier = Modifier.fillMaxWidth(),
+            title = "Get benefits near you",
+            description = "Activate coupons wherever you are, in different types of establishments",
+            iconRes = R.drawable.ic_ticket
         )
     }
 }
 
 @Preview
 @Composable
-private fun WelcomeHeaderPreview() {
-    WelcomeHeader()
+private fun WelcomeContentPreview() {
+    WelcomeContent()
 }
--- app/src/main/java/com/matheus/nearby/ui/components/welcome/WelcomeHowItWorksTip.kt
+++ app/src/main/java/com/matheus/nearby/ui/components/welcome/WelcomeHowItWorksTip.kt
@@ -1,2 +1,51 @@
 package com.matheus.nearby.ui.components.welcome
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.matheus.nearby.R
+import com.matheus.nearby.ui.theme.Gray500
+import com.matheus.nearby.ui.theme.RedBase
+import com.matheus.nearby.ui.theme.Typography
+
+@Composable
+fun WelcomeHowItWorksTip(
+    modifier: Modifier = Modifier,
+    title: String,
+    description: String,
+    @DrawableRes iconRes: Int
+) {
+    Row(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+        Icon(
+            modifier = Modifier.size(32.dp),
+            painter = painterResource(id = iconRes),
+            tint = RedBase,
+            contentDescription = "icon"
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(text = title, style = Typography.headlineSmall)
+            Text(text = description, style = Typography.bodyLarge, color = Gray500)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun WelcomeHowItWorksTipPreview() {
+    WelcomeHowItWorksTip(title = "hello", description = "description", iconRes = R.drawable.ic_map_pin)
+}
